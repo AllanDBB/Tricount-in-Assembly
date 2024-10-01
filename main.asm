c21b6b00407c2180852ef500d440da677927db06
Brian Ramirez <brianramirez01arias@gmail.com>
2024-09-30 19:27:45 -0600

    Test
    Testing createFolder and appendString

--- main.asm
+++ main.asm
@@ -7,20 +7,35 @@
 ;%include "createFolder.mac"   
 
 .DATA
- 
+    file_name1 db '/deudas.txt', 0
+    file_name2 db '/personas.txt', 0
+    file_name3 db '/o.txt', 0
 
 .UDATA
-    folder_name resb 1
+    folder_name resb 2
+
 .CODE
    ;TODO: Research the correct way to import funtions
-   extern createFolder  ; Declare the external function
+    extern createFolder      ; Declare the external function
+    ;extern createFile        ; Declare the external function
+    extern appendString      ; Declare the external function
 
     .STARTUP
+        ; Create folder
         GetStr folder_name,1
-        push folder_name     ; Push the address of the directory name onto the stack
-        call createFolder  ; Call the createFolder function
-        add esp, 4         ; Clean up the stack after the call
+        push folder_name     ; Push the address of the folder name onto the stack
+        call createFolder    ; Call the createFolder function
+        add esp, 4          ; Clean up the stack after the call
+
+        ; Crear archivo deudas.txt
+        push file_name1             ; Empujar la dirección del nombre del archivo
+        push folder_name            ; Empujar la dirección del nombre de la carpeta
+        call appendString           ; Llamar a la función appendString para concatenar
+        add esp, 8                  ; Limpiar la pila
+
+        PutStr folder_name
+        ;push folder_name            ; Empujar el nombre de carpeta + archivo
+        ;call createFile             ; Llamar a createFile
+        ;add esp, 4                  ; Limpiar la pila
 
     .EXIT
-
-
